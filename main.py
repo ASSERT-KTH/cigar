54ba1a7cbc857c8699ea95e04ff976696c63894a
David Hidvegi <hidvegidave@gmail.com>
2023-04-19 09:28:27 +0200

    Initial run of the algorithm on Lang project, results are in lang_summary.csv

--- main.py
+++ main.py
@@ -57,19 +57,20 @@ def main():
             print(f"Reproducing {project}-{bug_id}")
             bug = framework.reproduce_bug(project, bug_id)
 
+            first_plausible_path_try = -1
+            plausible_patches = []
+            repair_cost = 0
+            comment = ""
+
             if bug.line_change_count > 2:
                 print(f"Skipping {project}-{bug_id} because it has more than 2 line changes")
-                with open(summary_file_path, 'a', newline='') as csvfile:
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writerow({'framework': framework_name,
-                                     'project': project,
-                                     'bug_id': bug_id,
-                                     'first_plausible_patch_try': -1, 
-                                     'plausible_patch_count': 0,
-                                     'repair_cost': 0,
-                                     'conversation_length': max_conv_length,
-                                     'max_tries': max_tries,
-                                     'comment': "SKIP: multiple line changes"})
+                comment = "SKIP: multiple line changes"
+            elif "INFILL" not in bug.masked_buggy_code or len(bug.buggy_line) < 2:
+                print(f"Skipping {project}-{bug_id} because buggy line could not be found")
+                comment = "SKIP: buggy line not found"
+            elif len(bug.test_line) < 2 :
+                print(f"Skipping {project}-{bug_id} because test line could not be found")
+                comment = "SKIP: test line not found"
             else:
                 print(f"Repairing {project}-{bug_id}")
                 first_plausible_path_try, plausible_patches, repair_cost = capr.repair(bug=bug)
@@ -78,17 +79,17 @@ def main():
                     with open(f'{plausible_patches_folder}/{framework_name}_{project}_{bug_id}_pp.txt', 'w') as f:
                         f.writelines('\n'.join(plausible_patches))
                 
-                with open(summary_file_path, 'a', newline='') as csvfile:
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writerow({'framework': framework_name,
-                                     'project': project,
-                                     'bug_id': bug_id,
-                                     'first_plausible_patch_try': first_plausible_path_try, 
-                                     'plausible_patch_count': len(plausible_patches),
-                                     'repair_cost': repair_cost,
-                                     'conversation_length': max_conv_length,
-                                     'max_tries': max_tries,
-                                     'comment': ""})
+            with open(summary_file_path, 'a', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow({'framework': framework_name,
+                                 'project': project,
+                                 'bug_id': bug_id,
+                                 'first_plausible_patch_try': first_plausible_path_try, 
+                                 'plausible_patch_count': len(plausible_patches),
+                                 'repair_cost': repair_cost,
+                                 'conversation_length': max_conv_length,
+                                 'max_tries': max_tries,
+                                 'comment': comment})
 
 
 if __name__ == "__main__":
