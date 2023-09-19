603bb8558783b464c9d0c8a4121904ae1fa164b8
David Hidvegi <hidvegidave@gmail.com>
2023-09-19 21:11:45 +0200

    Fix typo in install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,3 @@
-### Install OS specific requirements
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Linux detected"
-    sudo apt install subversion
-    sudo apt install openjdk-8-jdk -y
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Mac detected"
-    brew install svn
-    brew install openjdk@8
-else
-    echo "OS not supported"
-    exit 1
-fi
-
 ### Install python requirements
 # Create virtualenv for the repo
 pip3 install virtualenv
@@ -25,13 +11,26 @@ pip install -r requirements.txt
 wget https://search.maven.org/remote_content\?g\=fr.inria.gforge.spoon.labs\&a\=gumtree-spoon-ast-diff\&v\=LATEST\&c\=jar-with-dependencies -O gumtree-spoon-ast-diff.jar
 # Download Defects4J
 git clone https://github.com/rjust/defects4j.git
+# Install subversion and java 8
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected"
+    sudo apt install subversion
+    sudo apt install openjdk-8-jdk -y
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Mac detected"
+    brew install svn
+    brew install openjdk@8
+else
+    echo "OS not supported"
+    exit 1
+fi
 
 ### Set parameters
 rm user_params.py
 cp user_params.py.template user_params.py
 user_params=$(<user_params.py)
 # set TMP_DIR to "/tmp/defects4j"
-user_params=${user_params//path_to_defects4j/\/tmp\/defects4j}
+user_params=${user_params//patch_to_tmp_dir/\/tmp\/defects4j}
 # set D4J_PATH to "$(pwd)/defects4j/framework/bin"
 user_params=${user_params//path_to_d4j_bin/$(pwd)\/defects4j\/framework\/bin}
 # set JAVA_HOME to $(/usr/libexec/java_home -version 1.8)
@@ -42,4 +41,5 @@ user_params=${user_params//path_to_java_home/$java_home}
 echo "$user_params" > user_params.py
 
 # Print that user should set API_KEY
+echo
 echo "Please set your API_KEY in user_params.py"
