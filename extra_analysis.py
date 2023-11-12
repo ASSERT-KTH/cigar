ec86e8993f8cea4c24fe3e7277c9b4379ea1d72b
Khashayar Etemadi <khaes@kth.se>
2023-11-12 20:54:55 +0100

    plots are drawn

--- extra_analysis.py
+++ extra_analysis.py
@@ -1,20 +1,33 @@
-import os
-import logging
 import json
+import matplotlib.pyplot as plt
 from pathlib import Path
 from prog_params import ProgParams as prog_params
 
+ROUND_INFO_FILE = f'output/defects4j_RapidCapr/rounds_info.json'
+
 def main():
-    compute_round_distinct_hashes()
+    compute_round_exact_matches()
+
+def plot_round_distinct_hashes():
+    rounds_info = list(json.load(open(ROUND_INFO_FILE, 'r')).values())
+    rounds_info.sort(key=lambda x:(x['round'], x['trycount']))
+    tokens = [sum([r['cost'] for r in rounds_info[:i+1]]) for i in range(len(rounds_info))]
+    distinct_hashes = [sum([r['distinct_hashes'] for r in rounds_info[:i+1]]) for i in range(len(rounds_info))]
+    distinct_plausible_hashes = [sum([r['distinct_plausible_hashes'] for r in rounds_info[:i+1]]) for i in range(len(rounds_info))]
+    plt.plot(tokens, distinct_hashes, label='#Distinct Hashes')
+    plt.plot(tokens, distinct_plausible_hashes, label='#Distinct Plausible Hashes')
+    plt.xlabel('Token_Cost')
+    plt.ylabel('Distinct Hashes')
+    plt.legend()
+    plt.show()
 
 def compute_round_distinct_hashes():
     patch_hash_to_test_res = get_patch_hash_to_test_result()
 
+    considered_bugs = set()
     previous_hashes = set()
-    round_info = {}
-    total_cost = 0
-    total_distinct_plausible = 0
-    total_distinct = 0
+    rounds_info = {}
+    first_plausible_try_info = {}
     pathlist = Path(prog_params.gpt35_cache_folder).glob('**/*.json')
     for path in pathlist:
         path_in_str = str(path)
@@ -28,20 +41,108 @@ def compute_round_distinct_hashes():
         current_hashes = set(json.load(open(path_in_str, 'r'))['patch_hashes'])
         distinct_hashes = current_hashes.difference(previous_hashes)
         current_cost = int(json.load(open(path_in_str.replace('_patch_hashes', ''), 'r'))['response']['usage']['total_tokens'])
-        total_cost += current_cost
 
         distinct_plausible_hashes = set(h for h in distinct_hashes if patch_hash_to_test_res[(proj, bug_id, h)] == 'PASS')
-        total_distinct_plausible += len(distinct_plausible_hashes)
-        total_distinct += len(distinct_hashes)
 
-        round_info[f'round: {round}, trycount: {trycnt}'] = {'distinct_hashes': len(distinct_hashes), 
-                                                             'distinct_plausible_hashes': len(distinct_plausible_hashes), 
-                                                             'current_cost': current_cost, 'total_distinct': total_distinct, 
-                                                             'total_distinct_plausible': total_distinct_plausible, 'total_cost': total_cost}
+        try_id = f"r{round:02d}_t{trycnt:02d}"
+
+        if try_id not in rounds_info:
+            rounds_info[try_id] = {'distinct_hashes': 0, 
+                                   'distinct_plausible_hashes': 0, 
+                                   'cost': 0,
+                                   'round': round, 'trycount': trycnt}
+        rounds_info[try_id]['distinct_hashes'] += len(distinct_hashes)
+        rounds_info[try_id]['distinct_plausible_hashes'] += len(distinct_plausible_hashes)
+        rounds_info[try_id]['cost'] += current_cost
+
+        proj_bug_id = f'{proj}_{bug_id}'
+
+        rounds_info[f'{try_id}_{proj_bug_id}'] = {'distinct_hashes': len(distinct_hashes),
+                                                    'distinct_plausible_hashes': len(distinct_plausible_hashes),
+                                                    'cost': current_cost,
+                                                    'proj': proj, 'bug_id': bug_id,
+                                                    'round': round, 'trycount': trycnt}
+        
+        considered_bugs.add(proj_bug_id)
+        
+        if round < first_plausible_try_info[proj_bug_id]['round'] or (round == first_plausible_try_info[proj_bug_id]['round'] and trycnt < first_plausible_try_info[proj_bug_id]['trycount']):
+            if any(patch_hash_to_test_res[(proj, bug_id, h)] == 'PASS' for h in current_hashes):
+                first_plausible_try_info[proj_bug_id] = {'round': round, 'trycount': trycnt}
+        
+        previous_hashes.update(current_hashes)
     
-    with open(f'output/defects4j_RapidCapr/rounds_info.json', "w") as file:
-                    json.dump(round_info, file,  indent=4, sort_keys=True)
 
+    rounds_info['first_plausible'] = {}
+    rounds_info['amplification'] = {}
+
+
+    first_plausible_try_info_items = first_plausible_try_info.items.sort(key=lambda x:(x[1]['round'], x[1]['trycount']))
+    min_try_bug = first_plausible_try_info_items[0][0]
+    max_try_bug = first_plausible_try_info_items[-1][0]
+    median_try_bug = first_plausible_try_info_items[len(first_plausible_try_info_items)//2][0]
+    no_plausible_bug = considered_bugs.difference(set(first_plausible_try_info.keys()))[0]
+    print(f'min_try: {min_try_bug}, max_try: {max_try_bug}, median_try: {median_try_bug}')
+
+
+
+    for item in rounds_info.values():
+        proj_bug_id = f'{item["proj"]}_{item["bug_id"]}'
+
+        if compare_try(item, first_plausible_try_info[proj_bug_id]) <= 0:
+            try_type = 'first_plausible'
+            try_id = f"r{item['round']:02d}_t{item['trycnt']:02d}"
+        else:
+            try_type = 'amplification'
+            try_id = f"r{item['try'] - first_plausible_try_info[proj_bug_id]['trycnt']}"
+
+        if not try_id in rounds_info[f'{try_type}']:
+            rounds_info[f'{try_type}'][try_id] = {'distinct_hashes': 0, 
+                                                        'distinct_plausible_hashes': 0, 
+                                                        'cost': 0,
+                                                        'round': item['round'], 'trycount': item['trycnt']}
+        rounds_info[f'{try_type}'][try_id]['distinct_hashes'] += item['distinct_hashes']
+        rounds_info[f'{try_type}'][try_id]['distinct_plausible_hashes'] += item['distinct_plausible_hashes']
+        rounds_info[f'{try_type}'][try_id]['cost'] += item['cost']
+
+        selected_bug_type = None
+
+        if proj_bug_id == min_try_bug:
+            selected_bug_type = 'min_try_bug'
+        elif proj_bug_id == max_try_bug:
+            selected_bug_type = 'max_try_bug'
+        elif proj_bug_id == median_try_bug:
+            selected_bug_type = 'median_try_bug'
+        elif proj_bug_id == no_plausible_bug:
+            selected_bug_type = 'no_plausible_bug'
+        
+        if selected_bug_type is not None:
+            if not try_type in rounds_info[selected_bug_type]:
+                rounds_info[selected_bug_type][try_type] = {}
+            if not try_id in rounds_info[selected_bug_type][try_type]:
+                rounds_info[selected_bug_type][try_type][try_id] = {'distinct_hashes': 0, 
+                                                                'distinct_plausible_hashes': 0, 
+                                                                'cost': 0,
+                                                                'round': item['round'], 'trycount': item['trycnt']}
+            rounds_info[selected_bug_type][try_type][try_id]['distinct_hashes'] += item['distinct_hashes']
+            rounds_info[selected_bug_type][try_type][try_id]['distinct_plausible_hashes'] += item['distinct_plausible_hashes']
+            rounds_info[selected_bug_type][try_type][try_id]['cost'] += item['cost']
+
+    
+    with open(ROUND_INFO_FILE, "w") as file:
+        json.dump(rounds_info, file,  indent=4, sort_keys=True)
+
+def compare_try(item1, item2):
+    if item1['round'] < item2['round']:
+        return -1
+    elif item1['round'] > item2['round']:
+        return 1
+    else:
+        if item1['trycount'] < item2['trycount']:
+            return -1
+        elif item1['trycount'] > item2['trycount']:
+            return 1
+        else:
+            return 0
 
 def get_patch_hash_to_test_result():
     ret = {}
