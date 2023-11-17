6a03b3c49bba11929ae8ae43055ae957b8cc2e3b
Khashayar Etemadi <khaes@kth.se>
2023-11-17 19:29:40 +0100

    round info per bug added

--- extra_analysis.py
+++ extra_analysis.py
@@ -4,51 +4,52 @@ from pathlib import Path
 from prog_params import ProgParams as prog_params
 
 ROUND_INFO_FILE = f'output/defects4j_RapidCapr/rounds_info.json'
+SELECTED_ROUND_INFO_FILE = f'output/defects4j_RapidCapr/selected_rounds_info.json'
 
 def main():
-    plot_round_distinct_hashes()
+    compute_rounds_info([('Lang', '21'), ('Math', '85'), ('Lang', '31'), ('Lang', '21')])
 
 def plot_round_distinct_hashes():
     fig, mplot = plt.subplots(2, 2)
 
     rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['first_plausible'])
     distinct_hashes = [sum([r['distinct_hashes'] for r in rounds_info[:i+1]]) for i in range(len(rounds_info))]
-    mplot[0][0].plot(tokens, distinct_hashes, label='First_Plausible')
-    for round_end_point in round_end_points:
-        mplot[0][0].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
+    mplot[0][0].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='First_Plausible')
+    # for round_end_point in round_end_points:
+    #     mplot[0][0].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
     mplot[0][0].set(xlabel='Token_Cost', ylabel='#Distinct_Patches')
     mplot[0][0].set_title("Plausible_Search")
 
     rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['amplification'])
     rounds_info, tokens, distinct_hashes = rounds_info[:5], tokens[:5], distinct_hashes[:5]
-    mplot[0][1].plot(tokens, distinct_hashes, label='Amplification')
+    mplot[0][1].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='Amplification')
     for i in range(len(tokens)):
-        mplot[0][1].plot([tokens[i]], [distinct_hashes[i]], "o", markersize=2, color="green")
+        mplot[0][1].plot([i + 1], [distinct_hashes[i]], "o", markersize=2, color="green")
     mplot[0][1].set(xlabel='Token_Cost', ylabel='#Distinct_Patches')
     mplot[0][1].set_title("Amplification")
 
-    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['min_try_bug', 'amplification'])
-    mplot[1][0].plot(tokens, distinct_hashes, label='Min_Try_Bug')
-    for i in range(len(tokens)):
-        mplot[1][0].plot([tokens[i]], [distinct_hashes[i]], "o", markersize=2, color="green")
-    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['max_try_bug', 'amplification'])
-    mplot[1][0].plot(tokens, distinct_hashes, label='Max_Try_Bug')
-    for i in range(len(tokens)):
-        mplot[1][0].plot([tokens[i]], [distinct_hashes[i]], "o", markersize=2, color="green")
+    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['max_try_bug', 'first_plausible'])
+    mplot[1][0].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='Max_Try_Bug')
+    # for round_end_point in round_end_points:
+    #     mplot[1][0].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
+    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['no_plausible_bug', 'first_plausible'])
+    mplot[1][0].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='No_Plausible_Bug')
+    # for round_end_point in round_end_points:
+    #     mplot[1][0].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
     mplot[1][0].set(xlabel='Token_Cost', ylabel='#Distinct_Patches')
-    mplot[1][0].set_title("Select_Amplification")
+    mplot[1][0].set_title("Select_Plausible_Search")
     mplot[1][0].legend()
 
-    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['max_try_bug', 'first_plausible'])
-    mplot[1][1].plot(tokens, distinct_hashes, label='Max_Try_Bug')
-    for round_end_point in round_end_points:
-        mplot[1][1].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
-    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['no_plausible_bug', 'first_plausible'])
-    mplot[1][1].plot(tokens, distinct_hashes, label='No_Plausible_Bug')
-    for round_end_point in round_end_points:
-        mplot[1][1].plot([round_end_point[0]], [round_end_point[1]], "o", markersize=2, color="black")
+    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['min_try_bug', 'amplification'])
+    mplot[1][1].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='Min_Try_Bug')
+    for i in range(len(tokens)):
+        mplot[1][1].plot([i + 1], [distinct_hashes[i]], "o", markersize=2, color="green")
+    rounds_info, tokens, distinct_hashes, round_end_points = get_cost_and_distinct_hashes(['max_try_bug', 'amplification'])
+    mplot[1][1].plot(range(1, len(distinct_hashes) + 1), distinct_hashes, label='Max_Try_Bug')
+    for i in range(len(tokens)):
+        mplot[1][1].plot([i + 1], [distinct_hashes[i]], "o", markersize=2, color="green")
     mplot[1][1].set(xlabel='Token_Cost', ylabel='#Distinct_Patches')
-    mplot[1][1].set_title("Select_Plausible_Search")
+    mplot[1][1].set_title("Select_Amplification")
     mplot[1][1].legend()
 
     for ax in mplot.flat:
@@ -79,7 +80,8 @@ def compute_rounds_info():
     previous_hashes = set()
     rounds_info = {}
     first_plausible_try_info = {}
-    pathlist = Path(prog_params.gpt35_cache_folder).glob('**/*.json')
+    pathlist = list(str(p) for p in Path(prog_params.gpt35_cache_folder).glob('**/*.json') if '_R' in str(p))
+    pathlist.sort(key=lambda x: (int(x.split('_R')[1].split('_')[0]), int(x.split('_R')[1].split('_')[1])))
     for path in pathlist:
         path_in_str = str(path)
         if not path_in_str.endswith(".json") or '_R' not in path_in_str or not 'patch_hashes' in path_in_str:
@@ -100,16 +102,19 @@ def compute_rounds_info():
         if try_id not in rounds_info:
             rounds_info[try_id] = {'distinct_hashes': 0, 
                                    'distinct_plausible_hashes': 0, 
+                                   'current_hashes': 0,
                                    'cost': 0,
                                    'round': round, 'trycount': trycnt}
         rounds_info[try_id]['distinct_hashes'] += len(distinct_hashes)
         rounds_info[try_id]['distinct_plausible_hashes'] += len(distinct_plausible_hashes)
+        rounds_info[try_id]['current_hashes'] += len(current_hashes)
         rounds_info[try_id]['cost'] += current_cost
 
         proj_bug_id = f'{proj}_{bug_id}'
 
         rounds_info[f'{try_id}_{proj_bug_id}'] = {'distinct_hashes': len(distinct_hashes),
                                                     'distinct_plausible_hashes': len(distinct_plausible_hashes),
+                                                    'current_hashes': len(current_hashes),
                                                     'cost': current_cost,
                                                     'proj': proj, 'bug_id': bug_id,
                                                     'round': round, 'trycount': trycnt}
@@ -155,12 +160,14 @@ def compute_rounds_info():
 
         if not try_id in rounds_info[f'{try_type}']:
             rounds_info[f'{try_type}'][try_id] = {'distinct_hashes': 0, 
-                                                        'distinct_plausible_hashes': 0, 
+                                                        'distinct_plausible_hashes': 0,
+                                                        'current_hashes': 0,
                                                         'cost': 0,
                                                         'proj': item['proj'], 'bug_id': item['bug_id'],
                                                         'round': item['round'], 'trycount': item['trycount']}
         rounds_info[f'{try_type}'][try_id]['distinct_hashes'] += item['distinct_hashes']
         rounds_info[f'{try_type}'][try_id]['distinct_plausible_hashes'] += item['distinct_plausible_hashes']
+        rounds_info[f'{try_type}'][try_id]['current_hashes'] += item['current_hashes']
         rounds_info[f'{try_type}'][try_id]['cost'] += item['cost']
 
         selected_bug_type = None
@@ -182,15 +189,70 @@ def compute_rounds_info():
                 rounds_info[selected_bug_type][try_type] = {}
             if not try_id in rounds_info[selected_bug_type][try_type]:
                 rounds_info[selected_bug_type][try_type][try_id] = {'distinct_hashes': 0, 
-                                                                'distinct_plausible_hashes': 0, 
+                                                                'distinct_plausible_hashes': 0,
+                                                                'current_hashes': 0,
                                                                 'cost': 0,
                                                                 'round': item['round'], 'trycount': item['trycount']}
             rounds_info[selected_bug_type][try_type][try_id]['distinct_hashes'] += item['distinct_hashes']
             rounds_info[selected_bug_type][try_type][try_id]['distinct_plausible_hashes'] += item['distinct_plausible_hashes']
+            rounds_info[selected_bug_type][try_type][try_id]['current_hashes'] += item['current_hashes']
             rounds_info[selected_bug_type][try_type][try_id]['cost'] += item['cost']
 
     
     with open(ROUND_INFO_FILE, "w") as file:
+        json.dump(rounds_info, file,  indent=4, sort_keys=True)
+
+def compute_rounds_info(selected_bugs): # list[(proj, bug_id)]
+    patch_hash_to_test_res = get_patch_hash_to_test_result()
+
+    pathlist = list(str(p) for p in Path(prog_params.gpt35_cache_folder).glob('**/*.json') if '_R' in str(p))
+    rounds_info = {}
+    for bug in selected_bugs:
+        pathlist_for_bug = [p for p in pathlist if f'_{bug[0]}_{bug[1]}_' in p]
+        pathlist_for_bug.sort(key=lambda x: (int(x.split('_R')[1].split('_')[0]), int(x.split('_R')[1].split('_')[1])))
+
+        previous_hashes = set()
+
+        proj = bug[0]
+        bug_id = bug[1]
+        proj_bug_id = f'{proj}_{bug_id}'
+
+        rounds_info[f'{proj_bug_id}'] = {'first_plausible': {}, 'amplification': {}}
+
+        is_amplification = False
+
+        for path in pathlist_for_bug:
+            path = str(path)
+            if not path.endswith(".json") or '_R' not in path or not 'patch_hashes' in path:
+                continue
+
+            round = int(path.split('_R')[1].split('_')[0])
+            trycnt = int(path.split('_R')[1].split('_')[1])
+            current_hashes = set(json.load(open(path, 'r'))['patch_hashes'])
+            distinct_hashes = current_hashes.difference(previous_hashes)
+            current_cost = int(json.load(open(path.replace('_patch_hashes', ''), 'r'))['response']['usage']['total_tokens'])
+            plausible_hashes = set(h for h in current_hashes if patch_hash_to_test_res[(proj, bug_id, h)] == 'PASS')
+
+            try_id = f"r{round:02d}_t{trycnt:02d}"
+
+
+            if not is_amplification:
+                try_type = 'first_plausible'
+            else:
+                try_type = 'amplification'    
+            
+            rounds_info[f'{proj_bug_id}'][f'{try_type}'][f'{try_id}'] = {'distinct_hashes': len(distinct_hashes),
+                                                        'current_hashes': len(current_hashes),
+                                                        'cost': current_cost,
+                                                        'proj': proj, 'bug_id': bug_id,
+                                                        'round': round, 'trycount': trycnt}
+
+            previous_hashes.update(current_hashes)
+
+            if len(plausible_hashes) > 0:
+                is_amplification = True
+
+    with open(SELECTED_ROUND_INFO_FILE, "w") as file:
         json.dump(rounds_info, file,  indent=4, sort_keys=True)
 
 def compare_try(item1, item2):
