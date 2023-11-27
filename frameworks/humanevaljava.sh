7333c6916b599f7d49f73ac11cb01304c7b88193
André Silva <andre15andre@hotmail.com>
2023-11-27 17:54:14 +0100

    i hate bash

--- frameworks/humanevaljava.sh
+++ frameworks/humanevaljava.sh
@@ -38,7 +38,7 @@ function get_test_error {
     test_error=$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Failures:/{n;p}' | sed 's/\[ERROR\]//')
 
     #Output extracted data
-    echo $test_error
+    echo "$test_error"
 }
 
 function get_buggy_lines {
@@ -46,17 +46,18 @@ function get_buggy_lines {
     work_dir=$3
     cd $work_dir
 
-    sed -i 's/package humaneval\\.correct/package humaneval\\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
-
     IFS='∫' # preserve white spaces in code
-    code=$(diff --unified src/main/java/humaneval/buggy/$bug_id.java src/main/java/humaneval/correct/$bug_id.java)
-    code=${code##*@@} # take the code from the last @@ sign until the end of file using
-    code=$(echo "$code" | sed '/^[^-]/d') # remove all lines that doesn't start with -
-    code=$(echo "$code" | sed 's/^[-]//') # replace the - sign with nothing
 
-    sed -i 's/package humaneval\\.buggy/package humaneval\\.correct/g' src/main/java/humaneval/correct/$bug_id.java
+    sed -i 's/package humaneval\.correct/package humaneval\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
+    code="$(diff --unified src/main/java/humaneval/buggy/$bug_id.java src/main/java/humaneval/correct/$bug_id.java)"
+    sed -i 's/package humaneval\.buggy/package humaneval\.correct/g' src/main/java/humaneval/correct/$bug_id.java
 
-    echo $code
+    code="${code##*@@}" # take the code from the last @@ sign until the end of file using
+    code="$(echo "$code" | sed '/^[^-]/d')" # remove all lines that doesn't start with -
+    code="$(echo "$code" | sed 's/^[-]//')" # replace the - sign with nothing
+
+
+    echo "$code"
 }
 
 function get_fixed_lines {
@@ -64,17 +65,18 @@ function get_fixed_lines {
     work_dir=$3
     cd $work_dir
 
-    sed -i 's/package humaneval\\.correct/package humaneval\\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
-
     IFS='∫' # preserve white spaces in code
-    code=$(diff --unified src/main/java/humaneval/buggy/$bug_id.java src/main/java/humaneval/correct/$bug_id.java)
-    code=${code##*@@} # take the code from the last @@ sign until the end of file using
-    code=$(echo "$code" | sed '/^[^+]/d') # remove all lines that doesn't start with +
-    code=$(echo "$code" | sed 's/^[+]//') # replace the + sign with nothing
 
-    sed -i 's/package humaneval\\.buggy/package humaneval\\.correct/g' src/main/java/humaneval/correct/$bug_id.java
+    sed -i 's/package humaneval\.correct/package humaneval\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
+    code="$(diff --unified src/main/java/humaneval/buggy/$bug_id.java src/main/java/humaneval/correct/$bug_id.java)"
+    sed -i 's/package humaneval\.buggy/package humaneval\.correct/g' src/main/java/humaneval/correct/$bug_id.java
 
-    echo $code
+    code="${code##*@@}" # take the code from the last @@ sign until the end of file using
+    code="$(echo "$code" | sed '/^[^+]/d')" # remove all lines that doesn't start with +
+    code="$(echo "$code" | sed 's/^[+]//')" # replace the + sign with nothing
+
+
+    echo "$code"
 }
 
 function get_test_name {
@@ -89,11 +91,11 @@ function get_test_name {
     test_error=$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Failures:/{n;p}' | sed 's/\[ERROR\]//')
 
     # Extract test name
-    test_name=${test_error%%:*} # Test name is the part of test_error before :
-    test_name=${test_name##*.} # Test name is the part of test_name after .
+    test_name="${test_error%%:*}" # Test name is the part of test_error before :
+    test_name="${test_name##*.}" # Test name is the part of test_name after .
 
     #Output extracted data
-    echo $test_name
+    echo "$test_name"
 }
 
 function get_test_suite {
@@ -105,13 +107,13 @@ function get_test_suite {
     timeout_seconds=600
 
     # Extract test error
-    test_error=$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Failures:/{n;p}' | sed 's/\[ERROR\]//')
+    test_error="$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Failures:/{n;p}' | sed 's/\[ERROR\]//')"
 
     # Extract test name
-    test_name=${test_error%%.*} # Test name is the part of test_error before :
+    test_name="${test_error%%.*}" # Test name is the part of test_error before :
 
     #Output extracted data
-    echo $test_name
+    echo "$test_name"
 }
 
 function get_test_line {
@@ -124,23 +126,23 @@ function get_test_line {
     timeout_seconds=600
 
     # Extract test error
-    test_error=$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Tests run:/{n;p;n;p;n;p}')
+    test_error="$(timeout $timeout_seconds mvn test -Dtest=TEST_$bug_id | sed -n '/\[ERROR\] Tests run:/{n;p;n;p;n;p}')"
 
     # Extract portion with file name and number
-    test_error=${test_error##*at} # take everything after "at"
-    file_name=${test_error##*\(} # take everything after "("
-    file_name=${file_name%%:*} # take everything before ":"
-    line_number=${test_error##*:} # take everything after ":"
-    line_number=${line_number%%)*} # take everything before ")"
+    test_error="${test_error##*at}" # take everything after "at"
+    file_name="${test_error##*\(}" # take everything after "("
+    file_name="${file_name%%:*}" # take everything before ":"
+    line_number="${test_error##*:}" # take everything after ":"
+    line_number="${line_number%%)*}" # take everything before ")"
 
     file_name="src/test/java/humaneval/$file_name"
 
     # if test_file_path is not empty and test_line_count is not empty
     if [ ! -z "$file_name" ] && [ ! -z "$line_number" ]; then
-        test_line=$(exec sed "${line_number}!d" $file_name)
+        test_line="$(exec sed "${line_number}!d" $file_name)"
     fi
 
-    echo $test_line
+    echo "$test_line"
 }
 
 function get_code {
@@ -149,13 +151,13 @@ function get_code {
 
     IFS='∫' # preserve white spaces in code
 
-    code_block=$(get_git_show_function_code $@) # Get function code from git show that contains the changes
+    code_block="$(get_git_show_function_code $@)" # Get function code from git show that contains the changes
 
-    code=$(echo "$code_block" | sed '/^[-]/d') # Remove lines that start with -
-    code=$(echo "$code" | sed '/^[+]/s/+/ /') # Replace + with space in lines that start with +
-    code=$(echo "$code" | sed 's/^[[:space:]]//') # Remove first leading white spaces
+    code="$(echo "$code_block" | sed '/^[-]/d')" # Remove lines that start with -
+    code="$(echo "$code" | sed '/^[+]/s/+/ /')" # Replace + with space in lines that start with +
+    code="$(echo "$code" | sed 's/^[[:space:]]//')" # Remove first leading white spaces
 
-    echo $code
+    echo "$code"
 }
 
 function get_masked_code {
@@ -164,22 +166,22 @@ function get_masked_code {
 
     IFS='∫' # preserve white spaces in code
 
-    code_block=$(get_git_show_function_code $@) # Get function code from git show that contains the changes
+    code_block="$(get_git_show_function_code $@)" # Get function code from git show that contains the changes
 
     # Replace the every line staring with - with INFILL
-    masked_code=$(echo "$code_block" | sed '/^[+-]/s/[+-].*/INFILL/')
+    masked_code="$(echo "$code_block" | sed '/^[+-]/s/[+-].*/INFILL/')"
     
     # Keep only the lines that start with INFILL
-    number_of_lines_starting_with_infill=$(echo "$masked_code" | grep -c INFILL) # Count the number of lines starting with INFILL
+    number_of_lines_starting_with_infill="$(echo "$masked_code" | grep -c INFILL)" # Count the number of lines starting with INFILL
     for (( i=2; i<=$number_of_lines_starting_with_infill; i++ )); do
-        first_infill_line_count=$(echo "$masked_code" | grep -n INFILL | sed '1!d' | cut -d: -f1) # Get the line number of the first line starting with INFILL
-        masked_code=$(echo "$masked_code" | sed "${first_infill_line_count}d") # remove the line at first_infill_line_count
+        first_infill_line_count="$(echo "$masked_code" | grep -n INFILL | sed '1!d' | cut -d: -f1)" # Get the line number of the first line starting with INFILL
+        masked_code="$(echo "$masked_code" | sed "${first_infill_line_count}d")" # remove the line at first_infill_line_count
     done
 
-    masked_code=$(echo "$masked_code" | sed 's/INFILL/>>> [ INFILL ] <<</')
-    masked_code=$(echo "$masked_code" | sed 's/^[[:space:]]//') # Remove first leading white spaces
+    masked_code="$(echo "$masked_code" | sed 's/INFILL/>>> [ INFILL ] <<</')"
+    masked_code="$(echo "$masked_code" | sed 's/^[[:space:]]//')" # Remove first leading white spaces
 
-    echo $masked_code
+    echo "$masked_code"
 }
 
 function get_fixed_code {
@@ -188,13 +190,13 @@ function get_fixed_code {
 
     IFS='∫' # preserve white spaces in code
 
-    code_block=$(get_git_show_function_code $@) # Get function code from git show that contains the changes
+    code_block="$(get_git_show_function_code $@)" # Get function code from git show that contains the changes
 
-    code=$(echo "$code_block" | sed '/^[+]/d') # Remove lines that start with +
-    code=$(echo "$code" | sed '/^[-]/s/-/ /') # Replace - with space in lines that start with -
-    code=$(echo "$code" | sed 's/^[[:space:]]//') # Remove first leading white spaces
+    code="$(echo "$code_block" | sed '/^[+]/d')" # Remove lines that start with +
+    code="$(echo "$code" | sed '/^[-]/s/-/ /')" # Replace - with space in lines that start with -
+    code="$(echo "$code" | sed 's/^[[:space:]]//')" # Remove first leading white spaces
 
-    echo $code
+    echo "$code"
 }
 
 function validate_patch {
@@ -213,16 +215,16 @@ function validate_patch {
     if [[ $mode == "SF" ]]; then
         patch_function=$patch
     else
-        masked_code=$(get_masked_code $@)
+        masked_code="$(get_masked_code $@)"
         patch_function="${masked_code//\>\>\> \[ INFILL \] \<\<\</${patch}}"
     fi
 
     # Extract buggy code path
-    code_file_path=$(get_source_code_file_path $@)
+    code_file_path="$(get_source_code_file_path $@)"
 
     # Put patch_function in place of the buggy function in code_file_path
-    full_code=$(less $code_file_path)    
-    code_function=$(get_code $@)
+    full_code="$(less $code_file_path)"
+    code_function="$(get_code $@)"
     code_function_length=$(echo "$code_function" | wc -l | sed 's/^[ \t]*//;s/[ \t]*$//')
     code_function_first_line=$(echo "$code_function" | head -n 1)
 
@@ -262,25 +264,13 @@ function get_patch_git_diff {
     cd $work_dir
 
     # Get code path
-    code_file_path=$(get_source_code_file_path $@)
+    code_file_path="$(get_source_code_file_path $@)"
 
-    # export diff file of code_file_path to diff_file_path
-    echo $(git diff $code_file_path)
+    # export diff file of code_file_path to diff_file_pathget_code
+    echo "$(git diff $code_file_path)"
 }
 
 # ---------- UTILS -------------
-
-function get_test_suite_and_name {
-    work_dir=$3
-    cd $work_dir
-
-    # Extract test suite and test name
-    test_suite_and_name=$(exec sed '1!d' $work_dir/failing_tests | grep "\---") # Grep first line of failing_tests
-    test_suite_and_name=${test_suite_and_name/\---//} # Remove "---"
-    test_suite_and_name=${test_suite_and_name///} # Remove "empty spaces"
-
-    echo $test_suite_and_name
-}
 
 function get_source_code_file_path {
     bug_id=$2
@@ -292,22 +282,22 @@ function get_source_code_file_path {
 
 function is_list_continuous {
     list=$1
-    list_len=$(echo "$list" | wc -l | sed 's/^[ \t]*//;s/[ \t]*$//')
+    list_len="$(echo "$list" | wc -l | sed 's/^[ \t]*//;s/[ \t]*$//')"
     is_continuous=1
     for ((i=1;i<$list_len;i++)); do
-        list_i=$(echo ${list} | head -n ${i} | tail -n 1)
-        list_i_p1=$(echo ${list} | head -n $((i + 1)) | tail -n 1)
-        if [[ $((list_i + 1)) != $((list_i_p1)) ]]; then
+        list_i="$(echo ${list} | head -n ${i} | tail -n 1)"
+        list_i_p1="$(echo ${list} | head -n $((i + 1)) | tail -n 1)"
+        if [[ "$((list_i + 1))" != "$((list_i_p1))" ]]; then
             is_continuous=0
             break
         fi
     done
-    echo $is_continuous
+    echo "$is_continuous"
 }
 
 function is_function_line {
     line=$1
-    trimmed_line=$(echo "${line}" | sed 's/^[ \t]*//') # remove empty white spaces from beginning of line
+    trimmed_line="$(echo "${line}" | sed 's/^[ \t]*//')" # remove empty white spaces from beginning of line
     is_function=0
 
     if [[ $trimmed_line != "//"* ]]; then
@@ -320,7 +310,7 @@ function is_function_line {
         fi
     fi
 
-    echo ${is_function}
+    echo "${is_function}"
 }
 
 function get_function_line_from_line_in_code_block {
@@ -333,7 +323,7 @@ function get_function_line_from_line_in_code_block {
         fi
     done
 
-    echo ${line}
+    echo "${line}"
 }
 
 function get_function_line_count_from_line_in_code_block {
@@ -347,7 +337,7 @@ function get_function_line_count_from_line_in_code_block {
         fi
     done
     
-    echo ${line_count}
+    echo "${line_count}"
 }
 
 function get_first_change_line_count_number {
@@ -357,17 +347,17 @@ function get_first_change_line_count_number {
 
     IFS='∫'
 
-    sed -i 's/package humaneval\\.correct/package humaneval\\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
-    git_show=$(diff --unified src/main/java/humaneval/correct/$bug_id.java src/main/java/humaneval/buggy/$bug_id.java)
-    sed -i 's/package humaneval\\.buggy/package humaneval\\.correct/g' src/main/java/humaneval/correct/$bug_id.java
-    
-    first_change_line_count_number=$(echo "$git_show" | grep "^@@" | head -n 2 | tail -n 1) # take the second line that starts with @@
-    first_change_line_count_number=${first_change_line_count_number//@@/} # remove @@
-    first_change_line_count_number=$(echo "${first_change_line_count_number}" | sed 's/^[ \t]*//') # remove space from beginning of line
-    first_change_line_count_number=$(echo "${first_change_line_count_number}" | cut -d' ' -f1) # remove everything after the first space
-    first_change_line_count_number=$(echo "${first_change_line_count_number}" | sed 's/^[+-]//')
+    sed -i 's/package humaneval\.correct/package humaneval\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
+    git_show="$(diff -U0 src/main/java/humaneval/correct/$bug_id.java src/main/java/humaneval/buggy/$bug_id.java)"
+    sed -i 's/package humaneval\.buggy/package humaneval\.correct/g' src/main/java/humaneval/correct/$bug_id.java
 
-    echo $first_change_line_count_number
+    first_change_line_count_number="$(echo "$git_show" | grep "^@@" | head -n 1)" # take the first line that starts with @@
+    first_change_line_count_number="${first_change_line_count_number//@@/}" # remove @@
+    first_change_line_count_number="$(echo "${first_change_line_count_number}" | sed 's/^[ \t]*//')" # remove space from beginning of line
+    first_change_line_count_number="$(echo "${first_change_line_count_number}" | cut -d' ' -f1)" # remove everything after the first space
+    first_change_line_count_number="$(echo "${first_change_line_count_number}" | sed 's/^[+-]//')"
+
+    echo "$first_change_line_count_number"
 }
 
 function get_git_show_function_code {
@@ -377,18 +367,18 @@ function get_git_show_function_code {
     work_dir=$3
     cd $work_dir
 
-    sed -i 's/package humaneval\\.correct/package humaneval\\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
-    git_show=$(diff --unified src/main/java/humaneval/correct/$bug_id.java src/main/java/humaneval/buggy/$bug_id.java)
-    sed -i 's/package humaneval\\.buggy/package humaneval\\.correct/g' src/main/java/humaneval/correct/$bug_id.java
-    git_diff_code=${git_show##*@@} # take the code from the last @@ sign until the end of file using
-    code_block=$(echo $git_diff_code)
+    sed -i 's/package humaneval\.correct/package humaneval\.buggy/g' src/main/java/humaneval/correct/$bug_id.java
+    git_show="$(diff -U150 src/main/java/humaneval/correct/$bug_id.java src/main/java/humaneval/buggy/$bug_id.java)"
+    sed -i 's/package humaneval\.buggy/package humaneval\.correct/g' src/main/java/humaneval/correct/$bug_id.java
+    git_diff_code="${git_show##*@@}" # take the code from the last @@ sign until the end of file using
+    code_block="$(echo "$git_diff_code")"
 
     # get first line that starts with + or -
-    code_change_line_count=$(echo "$code_block" | grep -n -m 1 "^[+-]" | cut -d: -f1)
+    code_change_line_count="$(echo "$code_block" | grep -n -m 1 "^[+-]" | cut -d: -f1)"
 
     # Find code_start_line_count
-    code_start_line=$(get_function_line_from_line_in_code_block $code_change_line_count)
-    code_start_line_count=$(get_function_line_count_from_line_in_code_block $code_change_line_count)
+    code_start_line="$(get_function_line_from_line_in_code_block $code_change_line_count)"
+    code_start_line_count="$(get_function_line_count_from_line_in_code_block $code_change_line_count)"
 
     # Construct end_symbol
     start_line=${code_start_line}
@@ -398,20 +388,20 @@ function get_git_show_function_code {
             break
         fi
     done
-    end_symbol=$(echo "${start_line:0:$start_line_index}}") # take the substring of start_line from 0 to start_line_index and append }
+    end_symbol="$(echo "${start_line:0:$start_line_index}}")" # take the substring of start_line from 0 to start_line_index and append }
 
     # Find code_end_line_count
     for ((i=code_change_line_count;i<=((code_change_line_count + 1000));i++)); do # for loop that goes from code_change_line_count up to the end of the file
-        line=$(echo "$code_block" | sed "${i}!d") # get line i from code_block
+        line="$(echo "$code_block" | sed "${i}!d")" # get line i from code_block
         if [[ $line == "${end_symbol}"* ]]; then # if line contains end_symbol
             code_end_line_count=$i # set code_end_line_count to i
             break # break the loop
         fi
     done
 
-    code_block=$(echo "$code_block" | sed -n "${code_start_line_count},${code_end_line_count}p") # take the lines from code_start_line_count to code_end_line_count from code_block
+    code_block="$(echo "$code_block" | sed -n "${code_start_line_count},${code_end_line_count}p")" # take the lines from code_start_line_count to code_end_line_count from code_block
 
-    echo ${code_block}
+    echo "${code_block}"
 }
 
 # From https://unix.stackexchange.com/questions/43340/how-to-introduce-timeout-for-shell-scripting
